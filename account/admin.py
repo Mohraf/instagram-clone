eeedf254b094524690f5272b898804a729662a14
mohraf <amosngok@gmail.com>
2022-03-15 21:03:30 +0300

    add user admin

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+
 from .models import *
+from .forms import UserCreationForm, UserChangeForm
+
+
+class UserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = ((None, {
+        'classes': ('wide',),
+        'fields': ('email', 'date_of_birth', 'password1', 'password2')}),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Connection)
