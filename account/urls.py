1812aebd318f65246a68c944530dc9292a601493
mohraf <amosngok@gmail.com>
2022-03-15 15:07:14 +0300

    edit urls

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'account'
+
 urlpatterns = [
   path('login/', views.LoginView.as_view(), name="login"),
   path('signup/', views.SignUpView.as_view(), name='signup'),
