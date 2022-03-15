b5fede6c21df33fd39b57bd04aeb12b2bd57aa68
mohraf <amosngok@gmail.com>
2022-03-15 11:21:35 +0300

    update urls

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,47 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-  path('login', views.LoginView.as_view(), name="login"),
+  path('login/', views.LoginView.as_view(), name="login"),
+  path('signup/', views.SignUpView.as_view(), name='signup'),
+  path('logout/', views.logout_view, name='logout'),
+  path(
+      'users/',
+      views.ListAccountView.as_view(),
+      name='user_list'
+  ),
+  re_path(
+      r'^(?P<username>[-\w]{5,30})/$',
+      views.DetailAccountView.as_view(),
+      name='profile'
+  ),
+  re_path(
+      r'^(?P<username>[-\w]{5,30})/update/$',
+      views.UpdateAccountView.as_view(),
+      name='update'
+  ),
+  re_path(
+      r'^(?P<username>[-\w]{5,30})/update/password/$',
+      views.ChangePasswordView.as_view(),
+      name='change_password'
+  ),
+  re_path(
+      r'^(?P<username>[-\w]{5,30})/followers$',
+      views.FollowersListView.as_view(),
+      name='followers'
+  ),
+  re_path(
+      r'^(?P<username>[-\w]{5,30})/following/$',
+      views.FollowingListView.as_view(),
+      name='following'
+  ),
+  re_path(
+      r'^(?P<username>[-\w]{5,30})/follow/$',
+      views.follow_view,
+      name='follow'
+  ),
+  re_path(
+      r'^(?P<username>[-\w]{5,30})/unfollow/$',
+      views.unfollow_view,
+      name='unfollow'
+  ),
 ]
--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,37 @@
 from django.urls import path, re_path
 from . import views
 
+
+
 urlpatterns = [
+  path(
+        'create/',
+        views.CreatePostView.as_view(),
+        name='create'
+    ),
+  re_path(
+      r'(?P<slug>[\w\-]{10})/$',
+      views.DetailPostView.as_view(),
+      name='view'
+  ),
+  re_path(
+      r'(?P<slug>[\w\-]{10})/update/$',
+      views.UpdatePostView.as_view(),
+      name='update'
+  ),
+  re_path(
+      r'(?P<slug>[\w\-]{10})/delete/$',
+      views.DeletePostView.as_view(),
+      name='delete'
+  ),
+  re_path(
+      r'(?P<slug>[\w\-]{10})/like/$',
+      views.like_post_view,
+      name='like'
+  ),
+  re_path(
+      r'(?P<slug>[\w\-]{10})/unlike/$',
+      views.unlike_post_view,
+      name='unlike'
+  ),
 ]
